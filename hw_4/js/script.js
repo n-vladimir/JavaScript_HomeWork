b22fc51f06708f14032164f33a245243ed05e99b
n-vladimir <80723647+n-vladimir@users.noreply.github.com>
2021-04-22 19:07:06 +0300

    add task 2/3 and 3/3

--- hw_4/js/script.js
+++ hw_4/js/script.js
@@ -90,7 +90,113 @@ new Student({
 console.log(Student.listOfStudents);
 
 //-------------------2
+class CustomString {
+    constructor() {
 
+    }
+    reverse (string) {
+        // if (isNaN(string) == true) {
+        if ((typeof string) == 'string') {
+                let str = ''
+                for(let i = string.length - 1; i >= 0; i--){
+                    str +=string[i]
+                }
+                return str
+            } else {
+            console.warn('Значения должно быть строкой')
+        }
+    }
+    ucFirst (string) {
+       if (isNaN(string) == true) {
+           let str = ''
+           for(let i = 0; i < 1; i++) {
+               str += string[i].toUpperCase()
+           }
+           for(let i = 1; i < string.length; i++) {
+               str += string[i]
+           }
+           return str
+       }  else {
+           console.warn('Значения должно быть строкой')
+       }
+    }
+    ucWords (string) {
+        if (isNaN(string) == true) {
+            let str = '';
+            let arr = string.split(' ');
+            for(let value of arr) {
+            for(let i = 0; i < value.length; i++) {
+                if (i == 0) {
+                    str += ' ' + value[i].toUpperCase();
+                    continue;
+                }
+                str += value[i]
+            }
+            }
+            return str
+        }  else {
+            console.warn('Значение должно быть строкой')
+        }
+    }
+
+
+}
+let myString = new CustomString();
+
+console.log(myString.reverse('Hello'))
+console.log(myString.reverse(3333))
+console.log(myString.ucFirst('vasya'))
+console.log(myString.ucWords('vasya hello string upper'))
+
+//-------------3
+
+class Validator {
+    constructor() {
+        this.checkIsEmail;
+        this.checkIsDomain;
+        this.checkIsDate;
+        this.checkIsPhone;
+
+    }
+    checkIsEmail(str) {
+        let arr = str.split('@')
+        if(arr.length !== 2) return false
+        if(arr[0].length < 3) return false
+        if(arr[1].length < 3) return false
+        return true;
+    }
+    checkIsDomain(str) {
+        let arr = str.split('.')
+        let domain = arr[arr.length - 1]
+        if(domain !== 'com') return false
+        return true
+    }
+    checkIsDate(str) {
+        let arr = str.split('.')
+        let age = new Date().getFullYear() - arr[arr.length - 1];
+        if(arr.length !== 3) return false
+        if(arr[0] > 31 || arr[0] < 1) return false
+        if(arr[1] > 12 || arr[1] < 1) return false
+        if(age > 100 || age <= -1) return false
+        return true
+    }
+    checkIsPhone(str) {
+        let arr = str.split(' ')
+        let arrNum = arr[2].split('-')
+        if(arr[0] !== '+38') return false
+        if(arr[1].length !== 5) return false
+        if(arr[2].length !== 9) return false
+        if(arrNum.length !== 3) return false
+        return true
+    }
+}
+
+let validator = new Validator();
+
+console.log(validator.checkIsEmail('vasya.pupkin@gmail.com')); // true
+console.log(validator.checkIsDomain('google.com')); // true
+console.log(validator.checkIsDate('10.01.2021')); // true
+console.log(validator.checkIsPhone('+38 (066) 937-99-92')); // если код страны Украинский
 
 
 
